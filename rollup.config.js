f30142d924c22e53ae9c1ba641a65ca78c40ee1d
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-08-29 22:41:18 -0500

    initial stuffs

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import fs from 'fs'
+
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'))
+
+export default {
+  entry: 'src/index.js',
+  external: ['react', 'prop-types'],
+  exports: 'named',
+  globals: { react: 'React', 'prop-types': 'PropTypes' },
+  useStrict: false,
+  sourceMap: true,
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve({
+      jsnext: false,
+      main: true,
+      browser: true
+    }),
+    commonjs({
+      ignoreGlobal: true,
+      include: 'node_modules/**'
+    })
+  ],
+  targets: [
+    { dest: pkg.main, format: 'cjs' },
+    { dest: pkg.module, format: 'es' },
+    { dest: pkg['umd:main'], format: 'umd', moduleName: pkg.name }
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withApollo } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class ApolloTote extends React.Component {
+  static defaultProps = {
+    lazy: false
+  }
+
+  static propTypes = {
+    lazy: PropTypes.bool,
+    query: PropTypes.func,
+    variables: PropTypes.object
+  }
+
+  willUnmount = false
+  query = null
+  state = {
+    loading: false,
+    value: undefined,
+    error: undefined
+  }
+
+  componentWillMount () {
+    this.query = gql(this.props.query)
+    if (this.props.lazy) return
+
+    this._runQuery()
+  }
+
+  componentWillUnmount () {
+    this.willUnmount = true
+  }
+
+  _runQuery = async () => {
+    this.setState({ loading: true })
+    if (this.willUnmount) return
+
+    const options = { query: this.query }
+
+    if (this.props.variables) {
+      options.variables = this.props.variables
+    }
+
+    try {
+      const result = await this.props.client.query(options)
+
+      if (result.error) {
+        throw result.error
+      }
+
+      this.setState({ loading: false, error: undefined, value: result.data })
+    } catch (error) {
+      this.setState({ loading: false, error, value: undefined })
+      return undefined
+    }
+  }
+
+  render () {
+    if (!this.props.render) return null
+    const { loading, value, error } = this.state
+
+    if (loading) {
+      if (!this.props.renderLoading) return null
+      return this.props.renderLoading()
+    }
+
+    return this.props.render({
+      getValue: this._getValue,
+      loading,
+      value,
+      error
+    })
+  }
+}
+
+export default withApollo(ApolloTote)
